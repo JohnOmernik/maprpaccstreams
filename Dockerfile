b178ffe93ea663ac34883ca61cb593d3b129cda1
johnomernik <john@omernik.com>
2018-10-25 10:33:00 -0500

    Added the ability to do LDAP lookups and add local users that may be part of the cluster

--- Dockerfile
+++ Dockerfile
@@ -4,10 +4,13 @@ FROM maprtech/pacc:6.1.0_6.0.0_ubuntu16
 
 ENV MAPR_STREAMS_PYTHON="http://package.mapr.com/releases/MEP/MEP-6.0.0/mac/mapr-streams-python-0.11.0.tar.gz"
 
+RUN adduser --disabled-login --gecos '' --uid=2000 mapr && adduser --disabled-login --gecos '' --uid=2500 zetaadm
+RUN usermod -a -G root mapr && usermod -a -G adm mapr && usermod -a -G disk mapr && usermod -a -G root zetaadm && usermod -a -G adm zetaadm && usermod -a -G disk zetaadm
 
-RUN apt-get update && apt-get install -y \
-              nano vim \
-              git \
+
+
+RUN apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y -qq \
+              nano vim git libnss3 libpam-ldap nscd \
               python3 python3-dev python3-setuptools python3-pip build-essential \
               zlib1g-dev libssl-dev libsasl2-dev liblz4-dev libsnappy1v5 libsnappy-dev liblzo2-2 liblzo2-dev \
               python-docutils \
@@ -25,8 +28,9 @@ RUN ln -s /opt/mapr/lib/librdkafka_jvm.so.1 /usr/lib/librdkafka_jvm.so && \
 
 RUN pip install python-snappy python-lzo brotli kazoo requests pytest && pip install --global-option=build_ext $MAPR_STREAMS_PYTHON
 
-#pip install --global-option=build_ext --global-option="--library-dirs=/opt/mapr/lib" --global-option="--include-dirs=/opt/mapr/include/" $MAPR_STREAMS_PYTHON
+ADD ./ldap.conf /etc/ldap.conf
 
+RUN DEBIAN_FRONTEND=noninteractive pam-auth-update && sed -i "s/compat/compat ldap/g" /etc/nsswitch.conf && /etc/init.d/nscd restart
 
 RUN mkdir -p /app/run
 
--- build.sh
+++ build.sh
@@ -9,13 +9,67 @@ echo "Using ENV file: $ENV_FILE"
 
 MYDIR=$(pwd)
 
+OUTLINE=""
+OUTLINEG=""
+
+for U in $MAPR_USER_LIST; do
+    U_NAME=$(echo -n "$U"|cut -d":" -f1)
+    U_ID=$(echo -n "$U"|cut -d":" -f2)
+
+    if [ "$U_ID" == "" ]; then
+        echo "Could not find user id for $U exiting"
+        exit 1
+    else
+        if [ "$OUTLINE" == "" ]; then
+            OUTLINE="RUN adduser --disabled-login --gecos '' --uid=${U_ID} ${U_NAME}"
+            OUTLINEG="RUN usermod -a -G root ${U_NAME} \&\& usermod -a -G adm ${U_NAME} \&\& usermod -a -G disk ${U_NAME}"
+        else
+            OUTLINE="${OUTLINE} \&\& adduser --disabled-login --gecos '' --uid=${U_ID} ${U_NAME}"
+            OUTLINEG="${OUTLINEG} \&\& usermod -a -G root ${U_NAME} \&\& usermod -a -G adm ${U_NAME} \&\& usermod -a -G disk ${U_NAME}"
+        fi
+    fi
+done
+if [ "$OUTLINE" == "" ]; then
+    sed -i -r "s@^RUN adduser.*@# RUN adduser @" ./Dockerfile
+    sed -i -r "s@^RUN usermod.*@# RUN usermod @" ./Dockerfile
+else
+    sed -i -r "s@^RUN adduser.*@# RUN adduser @" ./Dockerfile
+    sed -i -r "s@^RUN usermod.*@# RUN usermod @" ./Dockerfile
+    sed -i "s@# RUN adduser.*@$OUTLINE@" ./Dockerfile
+    sed -i "s@# RUN usermod.*@$OUTLINEG@" ./Dockerfile
+fi
+
 sed -i "s@FROM .*@FROM ${MAPR_PACC_IMG}@g" ./Dockerfile
+
+if [ "$MAPR_INCLUDE_LDAP" == "1" ]; then
+    sed -i "s/ libnss3 libpam-ldap nscd//g" ./Dockerfile
+    sed -i "s/ git/ git libnss3 libpam-ldap nscd/g" ./Dockerfile
+    # Setup LDAP information for OpenLDAP
+    echo "base ${MAPR_LDAP_BASE}" > ./ldap.conf
+    echo "uri ${MAPR_LDAP_URL}" >> ./ldap.conf
+    echo "binddn ${MAPR_LDAP_RO_USER}" >> ./ldap.conf
+    echo "bindpw ${MAPR_LDAP_RO_PASS}" >> ./ldap.conf
+    echo "ldap_version 3" >> ./ldap.conf
+    echo "pam_password md5" >> ./ldap.conf
+    echo "bind_policy soft" >> ./ldap.conf
+
+    sed -i "s@# ADD ./ldap.conf@ADD ./ldap.conf@g" ./Dockerfile
+    sed -i "s@# RUN DEBIAN_FRONTEND@RUN DEBIAN_FRONTEND@g" ./Dockerfile
+else
+    sed -i "s/ libnss3 libpam-ldap nscd//g" ./Dockerfile
+    sed -i -r "s@^ADD ./ldap.conf@# ADD ./ldap.conf@g" ./Dockerfile
+    sed -i -r "s@^RUN DEBIAN_FRONTEND@# RUN DEBIAN_FRONTEND@g" ./Dockerfile
+fi
+
+
+
+
 
 # Build maprpaccstreams
 sudo docker build -t $APP_IMG .
 
 if [ "$?" == "0" ]; then
-    echo "Image Build - Sucess!"
+    echo "Image Build - Success!"
 else
     echo "Image did not build correctly"
     exit 1
